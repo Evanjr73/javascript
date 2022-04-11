a7a0879f23c5ac1dc200691ad9dd03f9a7b92197
Evan junior <evsioljr@gmail.com>
2022-04-11 20:54:58 -0300

    projetos de relógio com javascript

--- aulas/aula12/ex014/escript.js
+++ aulas/aula12/ex014/escript.js
@@ -1,1 +1,18 @@
-alert('ola')
+function carregar(){
+    let msg = window.document.getElementById("msg")
+    let img = window.document.getElementById("img")
+    
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = 'agora são '+hora +'horas'
+    if (hora >= 0 && hora < 12){
+        //bom dai 
+        img.src = 'imagens/manhã2.png'
+    }else if (hora >= 12 && hora < 18){
+        //boa tarde//
+        img.src = 'imagens/tarde2.png'
+    }else{
+        //boa noite
+        img.src = 'imagens/noite2.png'
+    }
+}
